bb166f1bb629b502c3b3de3557094b8f3f44a1b2
Iewnfod <shangzhiqiuyue@outlook.com>
2024-05-24 12:51:40 +0800

    1.18.2a18

--- compile.py
+++ compile.py
@@ -34,7 +34,7 @@ override_ignore = [
 manifest = {
 	"name": "Tinkers-Construct",
 	"author": "Iewnfod",
-	"version": "1.18.2a17",
+	"version": "1.18.2a18",
 	"description": "",
 	"fileApi": "https://github.createchstudio.com/https://github.com/CreatechStudio/Tinkers-Construct/blob/main",
 	"files": {},
